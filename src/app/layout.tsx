136338cb339d0d0596feecec1bcfc481e7105ce2
Anjany Pandey <anjany.pandey06@gmail.com>
2025-02-16 14:42:43 +0530

    learned about file based routing and layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>This is a layout</h1>
         {children}
       </body>
     </html>
---
+++ src/app/performance/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>performance</div>;
+};
+
+export default page;
